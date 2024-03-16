8245e3fc0ddce4fea01d5efc664d94a4857edbb3
Maksat2222 <kersovmaks@gamil.com>
2024-03-16 20:47:26 +0300

    первая фиксация

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [objects, setObjects] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const [stop, setStop] = useState(false);
+
+  const createobject = () => {
+    for (let index = 0; index < 5; index++) {
+      const id = Math.floor(Math.random() * 1000);
+      const name = "Person" + (counter + 1);
+      const age = Math.floor(Math.random() * 50) + 20;
+      setObjects((prevObjects) => [...prevObjects, { id, name, age }]);
+      setCounter((prevCounter) => prevCounter + 1);
+    }
+  };
+
+  useEffect(() => {
+    let interval;
+    if (stop) {
+      interval = setInterval(() => {
+        createobject();
+        if (counter >= 5) {
+          setStop(false);
+        }
+      }, 2500);
+    }
+
+    return () => clearInterval(interval);
+  }, [counter, stop]);
+
+  const startAddingObjects = () => {
+    setStop(true);
+  };
+
+  const stopAddingObjects = () => {
+    setStop(false);
+  };
+  function hadlerDelete() {
+    setObjects((prevObjects) => prevObjects.slice(0, -5));
+  }
+
+  return (
+    <div>
+      <button onClick={startAddingObjects}>Start</button>
+      <button onClick={stopAddingObjects}>Stop</button>
+      <button onClick={hadlerDelete}>delete</button>
+      {objects.map((object) => (
+        <div key={object.id}>
+          <p>Имя: {object.name}</p>
+          <p>Возраст: {object.age}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
